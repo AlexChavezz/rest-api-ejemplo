76f057993319175a8fc2f98fe0438ba91ef121a4
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-08-23 19:37:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,15 @@ const app = express();
 const PORT = 8080;
 
 
+const corsOptions = {
+  origin: 'http://localhost',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 // Middlewares
 
 app.use(express.json())
-app.use(cors());
+app.use(cors(corsOptions));
 
 
 // Routes
