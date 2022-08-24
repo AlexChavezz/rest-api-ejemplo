64c8dad155d6718e202ebb72b36d6eddece7d370
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-08-23 19:55:18 -0500

    Create app.js

---
+++ build/app.js
@@ -0,0 +1,25 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+
+// Middlewares
+
+app.use(express.json())
+app.use(cors());
+
+
+// Routes
+
+app.get('/', (req, res) => {
+
+    // sacar la info de la db
+    // const array = [{name:"alexis"}, {}, {}]
+    // res.status(200).json(array)
+    res.send("hola mundo")
+})
+
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
