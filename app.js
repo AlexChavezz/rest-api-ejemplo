e5787693506d7f49fee05a68e934020d6b595c1d
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-08-23 19:40:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,10 @@ const app = express();
 const PORT = 8080;
 
 
-const corsOptions = {
-  origin: 'http://localhost',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
 // Middlewares
 
 app.use(express.json())
-app.use(cors(corsOptions));
+app.use(cors());
 
 
 // Routes
@@ -20,8 +15,9 @@ app.use(cors(corsOptions));
 app.get('/', (req, res) => {
 
     // sacar la info de la db
-    const array = [{name:"alexis"}, {}, {}]
-    res.status(200).json(array)
+    // const array = [{name:"alexis"}, {}, {}]
+    // res.status(200).json(array)
+    res.send("hola mundo")
 })
 
 app.listen(PORT, () => {
